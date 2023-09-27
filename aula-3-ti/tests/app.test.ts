8304e7180af84b516f5cb184cda68f59ce2e7eb6
Brendo <brendow_prime@hotmail.com>
2023-09-27 17:43:58 -0400

    aulas de 1 - 4

---
+++ aula-3-ti/tests/app.test.ts
@@ -0,0 +1,14 @@
+describe("operações básicas da matemática", () => {
+    it("deve retornar o valor da soma", () => {
+        expect(1+1).toEqual(2);
+    });
+    it("deve retornar o valor da subtração", () => {
+        expect(1-1).toEqual(0);
+    });
+    it("deve retornar o valor da mutiplicação", () => {
+        expect(2*2).toEqual(4);
+    });
+    it("deve retornar o valor da divisão", () => {
+        expect(4/2).toEqual(2);
+    });
+});
